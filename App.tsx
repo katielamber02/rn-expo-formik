b5a236060493c6e3300ca478a7a72387bf1fec76
Katie Lamber02 <katielamber02@gmail.com>
2020-02-09 10:21:26 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Form from './components/Form'
+import HBFormik from './components/HBFormik'
+
+export default function App() {
+  return (
+    <View >
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <HBFormik />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
